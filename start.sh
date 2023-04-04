54eb16790c9613ba181ece876580a2846c19c836
François Voron <fvoron@gmail.com>
2023-04-04 12:49:23 +0200

    Temporarily disable workspaces migrate

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ set -e
 # Run the release command in build until Render supports lifecycle hooks
 # See: https://community.render.com/t/release-command-for-db-migrations/247
 fief migrate
-fief workspaces migrate
+#fief workspaces migrate
 
 supervisord -c /etc/supervisord.conf
